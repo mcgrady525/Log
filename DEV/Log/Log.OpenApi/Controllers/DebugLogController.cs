ea46cde5918d81e8c93dbcacf5099580d07fe971
mcgrady_525 <mcgrady_525@163.com>
2017-01-23 18:03:38 +0800

    ...

--- DEV/Log/Log.OpenApi/Controllers/DebugLogController.cs
+++ DEV/Log/Log.OpenApi/Controllers/DebugLogController.cs
@@ -13,11 +13,21 @@ namespace Log.OpenApi.Controllers
     /// <summary>
     /// DebugLog调试日志
     /// </summary>
+    [RoutePrefix("api/debuglog")]
     public class DebugLogController : BaseController
     {
-        public void AddLog(DebugLog debugLog)
-        { 
+        /// <summary>
+        /// 添加日志
+        /// </summary>
+        /// <param name="debugLog"></param>
+        [Route("add")]
+        public HttpResponseMessage AddLog(DebugLog debugLog)
+        {
+            //将数据放到rabbitMQ消息队列中
 
+
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
         }
 
     }
