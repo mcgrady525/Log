3bda826314c765cd82c125ef5de8a5c09c042dcc
mcgrady_525 <mcgrady_525@163.com>
2017-01-25 14:43:59 +0800

    完成调试日志的AddLog方法。

---
+++ DEV/Log/Log.Common/Helper/RabbitMQHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Log.Common.Helper
+{
+    /// <summary>
+    /// RabbitMQ帮助类
+    /// </summary>
+    public class RabbitMQHelper
+    {
+
+    }
+}
--- DEV/Log/Log.OpenApi/Controllers/DebugLogController.cs
+++ DEV/Log/Log.OpenApi/Controllers/DebugLogController.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 using Tracy.Frameworks.LogClient.Entity;
 using Tracy.Frameworks.LogClient.Helper;
 using Tracy.Frameworks.Common.Extends;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace Log.OpenApi.Controllers
 {
@@ -21,11 +23,42 @@ namespace Log.OpenApi.Controllers
         /// </summary>
         /// <param name="debugLog"></param>
         [Route("add")]
+        [HttpPost]
         public HttpResponseMessage AddLog(DebugLog debugLog)
         {
             //将数据放到rabbitMQ消息队列中
+            var factory = new ConnectionFactory() { HostName = "127.0.0.1", Port = 5672, UserName = "admin", Password = "P@ssw0rd.123" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //声明交换机
+                channel.ExchangeDeclare(exchange: "Log.Exchange.DebugLog", type: ExchangeType.Direct, durable: true);
 
+                //声明队列
+                channel.QueueDeclare(queue: "Log.Queue.DebugLog",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
 
+                //绑定
+                channel.QueueBind(queue: "Log.Queue.DebugLog",
+                                  exchange: "Log.Exchange.DebugLog",
+                                  routingKey: "Log.Queue.DebugLog");
+
+                //持久化
+                var props = channel.CreateBasicProperties();
+                props.Persistent = true;
+
+                //发送消息
+                var jsonData = debugLog.ToJson();
+                var body = Encoding.UTF8.GetBytes(jsonData);
+                channel.BasicPublish(exchange: "Log.Exchange.DebugLog",
+                                     routingKey: "Log.Queue.DebugLog",
+                                     basicProperties: props,
+                                     body: body);
+
+            }
 
             return new HttpResponseMessage(HttpStatusCode.OK);
         }
