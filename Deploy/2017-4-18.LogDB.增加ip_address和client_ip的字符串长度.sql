afde3da1e0e1558fe0b0cb5aa8a60ab1e450a39b
mcgrady_525 <mcgrady_525@163.com>
2017-04-18 18:07:37 +0800

    add sql。

---
+++ Deploy/2017-4-18.LogDB.增加ip_address和client_ip的字符串长度.sql
@@ -0,0 +1,39 @@
+﻿USE LogDB;
+GO
+
+--增加ip_address和client_ip的字符串长度
+--debug log
+IF COL_LENGTH('t_logs_debug_log','ip_address') IS NOT NULL
+	ALTER TABLE dbo.t_logs_debug_log ALTER COLUMN ip_address NVARCHAR(512);
+go
+
+IF COL_LENGTH('t_logs_debug_log', 'client_ip') IS NOT NULL
+	ALTER TABLE dbo.t_logs_debug_log ALTER COLUMN client_ip NVARCHAR(512);
+go
+
+--error log
+IF COL_LENGTH('t_logs_error_log','ip_address') IS NOT NULL
+	ALTER TABLE dbo.t_logs_error_log ALTER COLUMN ip_address NVARCHAR(512);
+go
+
+IF COL_LENGTH('t_logs_error_log', 'client_ip') IS NOT NULL
+	ALTER TABLE dbo.t_logs_error_log ALTER COLUMN client_ip NVARCHAR(512);
+go
+
+--xml log
+IF COL_LENGTH('t_logs_xml_log','ip_address') IS NOT NULL
+	ALTER TABLE dbo.t_logs_xml_log ALTER COLUMN ip_address NVARCHAR(512);
+go
+
+IF COL_LENGTH('t_logs_xml_log', 'client_ip') IS NOT NULL
+	ALTER TABLE dbo.t_logs_xml_log ALTER COLUMN client_ip NVARCHAR(512);
+go
+
+--perf log
+IF COL_LENGTH('t_logs_performance_log','ip_address') IS NOT NULL
+	ALTER TABLE dbo.t_logs_performance_log ALTER COLUMN ip_address NVARCHAR(512);
+go
+
+IF COL_LENGTH('t_logs_performance_log', 'client_ip') IS NOT NULL
+	ALTER TABLE dbo.t_logs_performance_log ALTER COLUMN client_ip NVARCHAR(512);
+go
