0c747842655c8c00f5ef76b35cf8aeb04aafe914
mcgrady525 <mcgrady_525@163.com>
2017-06-23 15:07:23 +0800

    add XmlPerformanceLog api。

---
+++ DEV/Log/Log.Entity.RabbitMQ/AddXmlPerformanceLogRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Log.Entity.RabbitMQ
+{
+    /// <summary>
+    /// add xml日志和性能日志request
+    /// </summary>
+    public class AddXmlPerformanceLogRequest
+    {
+        public AddXmlLogRequest XmlLog { get; set; }
+
+        public AddPerformanceLogRequest PerformanceLog { get; set; }
+
+    }
+}
---
+++ DEV/Log/Log.OpenApi/Controllers/XmlPerformanceLogController.cs
@@ -0,0 +1,57 @@
+﻿using Log.Common.Helper;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tracy.Frameworks.RabbitMQ;
+using Tracy.Frameworks.Common.Extends;
+using Log.Entity.RabbitMQ;
+
+namespace Log.OpenApi.Controllers
+{
+    /// <summary>
+    /// xml和性能日志
+    /// </summary>
+    [RoutePrefix("api/xmlperformancelog")]
+    public class XmlPerformanceLogController : BaseController
+    {
+        private static IConnection rabbitMQConn = RabbitMQHelper.CreateConnection();
+        private static IRabbitMQWrapper _rabbitMQProxy;
+
+        public XmlPerformanceLogController(IRabbitMQWrapper rabbitMQProxy)
+        {
+            _rabbitMQProxy = rabbitMQProxy;
+        }
+
+        /// <summary>
+        /// 添加日志
+        /// </summary>
+        /// <param name="debugLog"></param>
+        [Route("add")]
+        [HttpPost]
+        public IHttpActionResult AddLog(List<AddXmlPerformanceLogRequest> list)
+        {
+            //校验
+            if (!list.HasValue())
+            {
+                return BadRequest();
+            }
+
+            using (var channel = rabbitMQConn.CreateModel())
+            {
+                foreach (var item in list)
+                {
+                    //分别写一条xml日志和一条性能日志
+                    _rabbitMQProxy.Publish(item.XmlLog, channel);
+                    _rabbitMQProxy.Publish(item.PerformanceLog, channel);
+                }
+            }
+
+            return Ok();
+        }
+
+    }
+}
